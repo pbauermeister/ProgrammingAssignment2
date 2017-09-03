8d5d71bf6ccf5b9e9dc631b4f467d79ed230bd9f
Pascal Bauermeister <pascal@privately.eu>
2017-09-03 23:05:10 +0200

    Implemented the assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,101 @@
-## Put comments here that give an overall description of what your
-## functions do
+## ---------------------------------------------------------------------------
+## R Programming: Coursera programming assignment number 2
+##
+## Work of Pascal Bauermeister - September 2017
+##
+## ---------------------------------------------------------------------------
+## Goal of the assignment: matrix inversion, with caching of results,
+## to have only the first access consuming CPU. Closure/environment
+## allow us to do the caching.
+## ---------------------------------------------------------------------------
 
-## Write a short comment describing this function
+
+## Factory function returning, for a given matrix, a list object
+## containing the matrix, its cached inverse, and access functions.
+## No computattion here, only caching.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    # data, additionally to x
+    inverse <- NULL
+        
+    # setters (note: as we do not provide a setter for x, the original
+    # matrix cannot change)
+    
+    setInverse <- function(m) inverse <<- m
+    
+    # getters
+    get <- function() x
+    getInverse <- function() inverse
+    
+    # bindings    
+    list(get=get, setInverse=setInverse, getInverse=getInverse)
 }
 
 
-## Write a short comment describing this function
+## Return the inverse of a cached matrix built by calling
+## makeCacheMatrix().  If the inverse has been previously computed and
+## cached, return the cached value, else solve the matrix and cache
+## it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    result <- x$getInverse() # maybe cached
+    if (!is.null(result)) {
+        message("  Getting cached data")
+    } else {
+        message("  Computing and caching inverse")
+        result <- solve(x$get())
+        x$setInverse(result)
+    }
+    result
+}
+
+
+##
+## Test the above with a big matrix.
+##
+
+test <- function() {
+    size <- 1500
+    m <- matrix(sample.int(100, size*size, replace=TRUE), size, size)
+    message("Original matrix")
+    print(m)
+
+    # access the inverse, and display benchmark
+    doit <- function() {
+        before <- Sys.time()
+        
+        inverse <- cacheSolve(cm)
+        
+        now <- Sys.time()
+        message("  Time: ", now-before)
+        message()
+    }
+
+    # fresh call
+    cm <- makeCacheMatrix(m)
+    message("1st call")
+    doit()
+    
+    # cached call
+    message("2nd call")
+    doit()
+    
+    # cached call
+    message("3rd call")
+    doit()
+
+    # fresh call again
+    cm <- makeCacheMatrix(m)
+    message("1st call, recreated cached matrix")
+    doit()
+    
+    # cached call
+    message("2nd call")
+    doit()
+    
+    # cached call
+    message("3rd call")
+    doit()
+    
+    "Test done"
 }
